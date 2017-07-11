6e10030d645cb87e15443118bfdb22eddd766656
jian <Hui6168@gmail.com>
2017-07-11 15:07:59 +0800

    swoole

---
+++ swoole/client.php
@@ -0,0 +1,28 @@
+<?php
+date_default_timezone_set("PRC");
+
+$client = new Swoole\Client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+
+// 连接
+$client->on("connect", function($cli) {
+	$message = date('H:i:s') . " hello world \n";
+    $cli->send($message);
+});
+
+// 接收
+$client->on("receive", function($cli, $data) {
+    echo "received: " . $data . "\n";
+});
+
+// 错误
+$client->on("error", function($cli) {
+    echo "connect failed \n";
+});
+
+// 断开
+$client->on("close", function($cli) {
+    echo "connection close \n";
+});
+
+// 连接
+$client->connect('127.0.0.1', 9501, 0.5);
---
+++ swoole/server.php
@@ -0,0 +1,39 @@
+<?php
+date_default_timezone_set("PRC");  
+
+// $serv = new Swoole\Server("127.0.0.1", 9501);
+$serv = new swoole_server("127.0.0.1", 9501);
+
+$serv->set([
+    'worker_num' => 2,   // 工作进程数量
+    // 'daemonize' => true, // 是否作为后台守护进程
+    'log_file' => './logs/' . date('Ymd') . '.log'
+]);
+
+// 启用主进程
+$serv->on('start', function($serv) {
+	// 设置主进程名称（macOS不支持）
+	// cli_set_process_title("swoole"); 
+	// 打印主进程的ID、管理进程的ID
+	echo date('H:i:s') . ' master_pid:' . $serv->master_pid. ', manager_pid:' . $serv->manager_pid ."\n";
+});
+
+// 连接
+$serv->on('connect', function ($serv, $fd){
+    echo date('H:i:s') ." client connect.\n";
+});
+
+// 接收
+$serv->on('receive', function ($serv, $fd, $from_id, $data) {
+	echo date('H:i:s') . " server receive fd:". $fd . " from_id:" . $from_id . " data:" . $data ."\n";
+    $serv->send($fd, 'server send data:'.$data);
+    $serv->close($fd);
+});
+
+// 断开
+$serv->on('close', function ($serv, $fd) {
+    echo date('H:i:s') . " client close.\n";
+});
+
+// 启动 主进程 + Manager进程 + worker_num个 Worker进程
+$serv->start();
